8c7b37c4a44f8d2651354c6f96449b9f9fdeaf79
Poker <poker_sang@outlook.com>
2025-11-07 23:31:00 +0800

    add AsyncImage

--- ItemsView.Avalonia.Sample/Control/ItemControl.axaml.cs
+++ ItemsView.Avalonia.Sample/Control/ItemControl.axaml.cs
@@ -1,6 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 using ItemsView.Avalonia.Sample.Models;
 using PropertyGenerator.Avalonia;
 
--- ItemsView.Avalonia.Sample/ViewModels/SampleViewModel.cs
+++ ItemsView.Avalonia.Sample/ViewModels/SampleViewModel.cs
@@ -1,154 +1,105 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using Avalonia.Layout;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ItemsView.Avalonia.Layouts.FlowLayout;
 using ItemsView.Avalonia.Layouts.MasonryLayout;
 using ItemsView.Avalonia.Sample.Models;
-using SixLabors.ImageSharp.Processing;
 using System.Collections;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using SixLabors.ImageSharp;
-using StringComparer = System.StringComparer;
 
 namespace ItemsView.Avalonia.Sample.ViewModels;
 
 public partial class SampleViewModel : ObservableObject
 {
     [ObservableProperty]
-    public partial IDataTemplate? ItemTemplate { get; set; }
+    public partial IDataTemplate? ItemTemplate { get; set; } = Application.Current?.FindResource("TextItemsTemplate") as IDataTemplate;
 
     [ObservableProperty]
-    public partial IEnumerable? Items { get; set; }
+    public partial IEnumerable? Items { get; set; } = TextItems;
+
+    public static IReadOnlyList<DataType> DataSources { get; } = Enum.GetValues<DataType>();
 
     [ObservableProperty]
-    public partial AttachedLayout? Layout { get; set; }
+    public partial DataType SelectedDataSource { get; set; } = DataType.TextItems;
+
+    public static IReadOnlyList<AttachedLayout> Layouts { get; } = [new StackLayout(), new FlowLayout(), new MasonryLayout(), new WrapLayout(), new UniformGridLayout()];
 
     [ObservableProperty]
-    public partial ObservableCollection<DataSource> DataSources { get; set; } = [];
+    public partial AttachedLayout? SelectedLayout { get; set; } = Layouts.FirstOrDefault();
 
-    [ObservableProperty]
-    public partial DataSource? SelectedDataSource { get; set; }
+    private static IReadOnlyList<Item>? TextItems => field ??= InitializeDataSources();
 
-    [ObservableProperty]
-    public partial ObservableCollection<AttachedLayout> Layouts { get; set; } = [];
-
-    [ObservableProperty]
-    public partial AttachedLayout? SelectedLayout { get; set; }
-
-    public SampleViewModel()
+    private static Item[] InitializeDataSources()
     {
-        InitializeDataSources();
-        InitializeLayouts();
-
-        SelectedDataSource = DataSources.FirstOrDefault();
-        SelectedLayout = Layouts.FirstOrDefault();
-    }
-
-    private void InitializeDataSources()
-    {
-        var itemTemplate = Application.Current!.FindResource("ItemTemplate") as IDataTemplate;
-        var imageTemplate = Application.Current!.FindResource("ImageTemplate") as IDataTemplate;
-
-        var items = new ObservableCollection<Item>();
+        var textItems = new Item[1000];
         for (var i = 0; i < 1000; i++)
         {
-            items.Add(new Item
+            textItems[i] = new()
             {
                 Value = i,
                 Name = $"Item {i}",
                 Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            });
+            };
         }
-        DataSources.Add(new DataSource { Name = "Default Items", Items = items, ItemTemplate = itemTemplate! });
-
-        var images = new ObservableCollection<Bitmap>();
-        DataSources.Add(new DataSource { Name = "Gallery Images", Items = images, ItemTemplate = imageTemplate! });
+        return textItems;
     }
 
-    private void InitializeLayouts()
+    partial void OnSelectedDataSourceChanged(DataType value)
     {
-        Layouts.Add(new StackLayout());
-        Layouts.Add(new FlowLayout());
-        Layouts.Add(new MasonryLayout());
-        Layouts.Add(new WrapLayout());
-        Layouts.Add(new UniformGridLayout());
+        ItemTemplate = Application.Current?.FindResource($"{value}Template") as IDataTemplate;
+
+        Items = value is DataType.TextItems ? TextItems : null;
     }
 
-    partial void OnSelectedDataSourceChanged(DataSource? value)
+    public void LoadFolder(string folder)
     {
-        if (value is null) return;
-
-        Items = value.Items;
-        ItemTemplate = value.ItemTemplate;
-    }
-
-    partial void OnSelectedLayoutChanged(AttachedLayout? value)
-    {
-        Layout = value;
-    }
-
-    public async Task LoadFolderAsync(string folder)
-    {
-        var dataSource = DataSources.FirstOrDefault(ds => ds.Name == "Gallery Images");
-        if (dataSource?.Items is not ObservableCollection<Bitmap> images) return;
-
-        images.Clear();
+        if (SelectedDataSource is not DataType.AsyncImageItems and not DataType.ImageItems)
+            return;
         var supported = new[] { ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".webp", ".tif", ".tiff" };
-        var files = Directory.EnumerateFiles(folder)
-            .Where(f => supported.Contains(Path.GetExtension(f), StringComparer.OrdinalIgnoreCase))
-            .ToArray();
-
-        foreach (var file in files)
+        var items = Directory.EnumerateFiles(folder)
+            .Where(f => supported.Contains(Path.GetExtension(f), StringComparer.OrdinalIgnoreCase));
+        Items = SelectedDataSource switch
         {
-            try
-            {
-                await using var stream = File.OpenRead(file);
-                using var image = await SixLabors.ImageSharp.Image.LoadAsync(stream);
+            DataType.AsyncImageItems => items.Select(t => new ImageUri(t)).ToArray(),
+            DataType.ImageItems => items.Select(t => Bitmap.DecodeToWidth(File.OpenRead(t), 300)).ToArray(),
+            _ => Items
+        };
 
-                const int thumbnailSize = 300;
-                var options = new ResizeOptions
-                {
-                    Size = new SixLabors.ImageSharp.Size(thumbnailSize, thumbnailSize),
-                    Mode = ResizeMode.Max
-                };
-                image.Mutate(x => x.Resize(options));
-
-                await using var memoryStream = new MemoryStream();
-                await image.SaveAsPngAsync(memoryStream);
-                memoryStream.Position = 0;
-
-                var bmp = new Bitmap(memoryStream);
-                images.Add(bmp);
-            }
-            catch
-            {
-                // ignore invalid images
-            }
-        }
+        // TODO: 先改folder source再换itemstemplate才能显示
+        var t = ItemTemplate;
+        ItemTemplate = null;
+        ItemTemplate = t;
     }
 
     [RelayCommand]
     public async Task OpenFolder()
     {
-        var dialog = new OpenFolderDialog();
-        var folder = await dialog.ShowAsync(MainWindow.Current);
-        if (!string.IsNullOrWhiteSpace(folder))
-        {
-            await LoadFolderAsync(folder);
-        }
+        if (TopLevel.GetTopLevel(MainWindow.Current) is not { } topLevel)
+            return;
+        var folder = await topLevel.StorageProvider.OpenFolderPickerAsync(new());
+        if (folder is not [{ } first] || first.TryGetLocalPath() is not { } path)
+            return;
+        LoadFolder(path);
     }
-
 }
-public class DataSource
+
+public record ImageUri(string Uri)
 {
-    public required string Name { get; set; }
-    public required IEnumerable Items { get; set; }
-    public required IDataTemplate ItemTemplate { get; set; }
+    public Task<Bitmap> BitmapAsync => Task.Run(() => Bitmap.DecodeToWidth(File.OpenRead(Uri), 300));
+}
+
+public enum DataType
+{
+    TextItems,
+    ImageItems,
+    AsyncImageItems
 }
