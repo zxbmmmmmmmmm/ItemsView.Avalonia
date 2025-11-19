be9a97f9ee19ee3970aa11c82a7dcab688654e74
Betta_Fish <zxbmmmmmmmmm@qq.com>
2025-11-19 16:02:11 +0800

    WIP

--- Virtualization.Avalonia.ItemsRepeater/Layouts/FlowLayout/FlowItem.cs
+++ Virtualization.Avalonia.ItemsRepeater/Layouts/FlowLayout/FlowItem.cs
@@ -8,22 +8,4 @@ internal class FlowItem(int index)
     public int Index { get; } = index;
 
     public Size? DesiredSize { get; internal set; }
-
-    public Size? Measure { get; internal set; }
-
-    public Point? Position { get; internal set; }
-
-    public Control? Element { get; internal set; }
-
-    public int IndexOfRow { get; internal set; }
-
-    public RowInfo RowInfo { get; internal set; } = null!;
 }
-
-internal class RowInfo(int itemCount = 0, double length = 0)
-{
-    public int ItemCount { get; set; } = itemCount;
-
-    public double Length { get; set; } = length;
-}
-
--- Virtualization.Avalonia.ItemsRepeater/Layouts/FlowLayout/FlowLayout.cs
+++ Virtualization.Avalonia.ItemsRepeater/Layouts/FlowLayout/FlowLayout.cs
@@ -40,12 +40,43 @@ public sealed partial class FlowLayout : VirtualizingLayout, IFlowLayoutAlgorith
         throw new NotImplementedException();
     }
 
-    Size IFlowLayoutAlgorithmDelegates.Algorithm_GetMeasureSize(int index, Size availableSize, VirtualizingLayoutContext context) => throw new NotImplementedException();
-    Size IFlowLayoutAlgorithmDelegates.Algorithm_GetProvisionalArrangeSize(int index, Size measureSize, Size desiredSize, VirtualizingLayoutContext context) => throw new NotImplementedException();
-    bool IFlowLayoutAlgorithmDelegates.Algorithm_ShouldBreakLine(int index, double remainingSpace) => throw new NotImplementedException();
-    FlowLayoutAnchorInfo IFlowLayoutAlgorithmDelegates.Algorithm_GetAnchorForRealizationRect(Size availableSize, VirtualizingLayoutContext context) => throw new NotImplementedException();
-    FlowLayoutAnchorInfo IFlowLayoutAlgorithmDelegates.Algorithm_GetAnchorForTargetElement(int targetIndex, Size availableSize, VirtualizingLayoutContext ccontext) => throw new NotImplementedException();
-    Rect IFlowLayoutAlgorithmDelegates.Algorithm_GetExtent(Size availableSize, VirtualizingLayoutContext context, Control firstRealized, int firstRealizedItemIndex, Rect firstRealizedLayoutBounds, Control lastRealized, int lastRealizedItemIndex, Rect lastRealizedLayoutBounds) => throw new NotImplementedException();
-    void IFlowLayoutAlgorithmDelegates.Algorithm_OnElementMeasured(Control element, int index, Size availableSize, Size measureSize, Size desiredSize, Size provisionalArrangeSize, VirtualizingLayoutContext context) => throw new NotImplementedException();
-    void IFlowLayoutAlgorithmDelegates.Algorithm_OnLineArranged(int startIndex, int countInLine, double lineSize, VirtualizingLayoutContext context) => throw new NotImplementedException();
+    Size IFlowLayoutAlgorithmDelegates.Algorithm_GetMeasureSize(int index, Size availableSize, VirtualizingLayoutContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    Size IFlowLayoutAlgorithmDelegates.Algorithm_GetProvisionalArrangeSize(int index, Size measureSize, Size desiredSize, VirtualizingLayoutContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IFlowLayoutAlgorithmDelegates.Algorithm_ShouldBreakLine(int index, double remainingSpace)
+    {
+        throw new NotImplementedException();
+    }
+
+    FlowLayoutAnchorInfo IFlowLayoutAlgorithmDelegates.Algorithm_GetAnchorForRealizationRect(Size availableSize, VirtualizingLayoutContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    FlowLayoutAnchorInfo IFlowLayoutAlgorithmDelegates.Algorithm_GetAnchorForTargetElement(int targetIndex, Size availableSize, VirtualizingLayoutContext ccontext)
+    {
+        throw new NotImplementedException();
+    }
+
+    Rect IFlowLayoutAlgorithmDelegates.Algorithm_GetExtent(Size availableSize, VirtualizingLayoutContext context, Control firstRealized, int firstRealizedItemIndex, Rect firstRealizedLayoutBounds, Control lastRealized, int lastRealizedItemIndex, Rect lastRealizedLayoutBounds)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IFlowLayoutAlgorithmDelegates.Algorithm_OnElementMeasured(Control element, int index, Size availableSize, Size measureSize, Size desiredSize, Size provisionalArrangeSize, VirtualizingLayoutContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IFlowLayoutAlgorithmDelegates.Algorithm_OnLineArranged(int startIndex, int countInLine, double lineSize, VirtualizingLayoutContext context)
+    {
+        throw new NotImplementedException();
+    }
 }
